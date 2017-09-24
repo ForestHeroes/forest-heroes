9b04545867cfaa8ba43fc6d1a3671a989c94dce6
Renan Barros <renanbarrosrbs@gmail.com>
2017-09-23 23:45:05 -0300

    dodge

--- Scripts/Attributes.gd
+++ Scripts/Attributes.gd
@@ -6,3 +6,10 @@ export(float) var DEF
 export(float) var ASPD
 export(float) var MSPD
 export(float) var ATKRANGE
+
+enum STATES{
+	IDLE,
+	DODGE
+}
+
+var current_state = STATES.IDLE
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,20 +1,25 @@
 extends KinematicBody2D
 
 onready var attributes = get_node("Attributes")
+onready var tween = get_node("Tween")
 
 const IDLE_SPEED = 10.0
 
 var current_anim = ""
 
-var status
 var speed = Vector2()
+var dodge_cd = 1
 
 func _ready():
-	status = get_node("Attributes")
 	set_fixed_process(true)
 	set_process_input(true)
 
 func _fixed_process(delta):
+	move_player(delta)
+	animate()
+
+func move_player(delta):
+	
 	var dir = Vector2()
 	if (Input.is_action_pressed("ui_up")):
 		dir += Vector2(0, -1)
@@ -25,20 +30,36 @@ func _fixed_process(delta):
 	if (Input.is_action_pressed("ui_right")):
 		dir += Vector2(1, 0)
 		
-
-	speed = dir.normalized() * attributes.MSPD
-	var motion = speed*delta
-	motion = move(motion)
+	if(Input.is_action_pressed("dodge") and dodge_cd >= 1):
+		dodge(get_pos(),dir.normalized())
+		yield( tween,"tween_complete")
+		dodge_cd = 0
+	dodge_cd += delta
+	
+	speed = dir.normalized() * attributes.MSPD * delta
+	
+	var motion = move(speed)
 	
 	if (is_colliding()):
 		var n = get_collision_normal()
 		motion = n.slide(motion)
 		move(motion)
 
+func dodge(start_pos,direction):
+	var distance = 100
+	var time = 0.2
+	var end_pos = start_pos + (direction * distance)
+	tween.interpolate_property(self,"transform/pos",
+	start_pos,
+	end_pos,
+	time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+
+func animate():
 	var next_anim = ""
 	var next_mirror = false
 	
-	if (dir == Vector2() and speed.length() < IDLE_SPEED):
+	if (speed.length() < IDLE_SPEED):
 		next_anim = "idle"
 	elif (speed.length() > IDLE_SPEED*0.1):
 		var angle = atan2(abs(speed.x), speed.y)
@@ -58,4 +79,7 @@ func _fixed_process(delta):
 	if (next_anim != current_anim):
 		#get_node("Anim").play(next_anim)
 		current_anim = next_anim
-		
+	
+
+func _on_Area2D_body_enter( body ):
+	tween.stop(self,"transform/pos")
